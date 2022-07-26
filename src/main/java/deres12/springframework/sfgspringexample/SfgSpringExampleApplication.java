056862a7484b5bddd01daaccb979323cf734ff69
Stefano Carrino <stefano.carrino@gruppomcr.com>
2022-07-26 12:42:44 +0200

    Add init file project spring

---
+++ src/main/java/deres12/springframework/sfgspringexample/SfgSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package deres12.springframework.sfgspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgSpringExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deres12/springframework/sfgspringexample/SfgSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package deres12.springframework.sfgspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
